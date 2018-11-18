f8e0259d4ebed9ebc719087c94d7f6c5b8744f54
packetChor <cs154061@mnnit.ac.in>
2018-11-18 15:33:28 +0530

    awesome list

--- flask_app/static/js/main.js
+++ flask_app/static/js/main.js
@@ -1,24 +1,19 @@
-$(document).scroll(function(){
-    $('#mainNav').toggleClass('scrolled', $(this).scrollTop() > 0);
+$(document).scroll(function () {
+	$('#mainNav').toggleClass('scrolled', $(this).scrollTop() > 0);
 });
 
 var jumboHeight = $('.jumbotron').outerHeight();
 
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('header').css('background-position-y', (-scrolled) + 'px');
+function parallax() {
+	var scrolled = $(window).scrollTop();
+	$('header').css('background-position-y', (-scrolled) + 'px');
 }
 
-$(window).scroll(function(e){
-    parallax();
+$(window).scroll(function (e) {
+	parallax();
 });
 
-
-$( "#dialog-link, #icons li" ).hover(
-	function() {
-		$( this ).addClass( "ui-state-hover" );
-	},
-	function() {
-		$( this ).removeClass( "ui-state-hover" );
-	}
-);
+$( function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  } );
