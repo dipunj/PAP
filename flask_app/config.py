21cf745d3dc2f5a6c71d1af1257980f1279b928e
packetChor <cs154061@mnnit.ac.in>
2018-11-28 19:34:47 +0530

    updated config

--- flask_app/config.py
+++ flask_app/config.py
@@ -2,6 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
     UPLOAD_FOLDER                  = os.path.join(basedir, 'db/')
     SQLALCHEMY_DATABASE_URI        = "sqlite:///{}".format(os.path.join(UPLOAD_FOLDER, "users.db"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
